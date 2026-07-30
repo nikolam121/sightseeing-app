f6fa1cbeac1f61ce00d0da8f865ae403f34ccce9
Gordan Caratan <gordancaratan@gmail.com>
2026-07-30 09:18:43 +0200

    initial commit

---
+++ src/test/java/hr/tis/academy/sightseeingapp/SightSeeingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.tis.academy.sightseeingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SightSeeingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
