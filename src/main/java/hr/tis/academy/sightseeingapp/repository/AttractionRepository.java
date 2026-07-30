a0f9d8135e3967f3298e0d93dae51fc50327f27d
Gordan Caratan <gordancaratan@gmail.com>
2026-07-30 23:15:06 +0200

    various fixes 5

---
+++ src/main/java/hr/tis/academy/sightseeingapp/repository/AttractionRepository.java
@@ -0,0 +1,9 @@
+package hr.tis.academy.sightseeingapp.repository;
+
+import hr.tis.academy.sightseeingapp.model.Attraction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AttractionRepository extends JpaRepository<Attraction, Long> {
+    Attraction findByName(String name);
+    boolean existsByName(String name);
+}
--- src/main/java/hr/tis/academy/sightseeingapp/service/impl/FavouriteServiceImpl.java
+++ src/main/java/hr/tis/academy/sightseeingapp/service/impl/FavouriteServiceImpl.java
@@ -5,6 +5,7 @@ import hr.tis.academy.sightseeingapp.mapper.FavouriteMapper;
 import hr.tis.academy.sightseeingapp.mapper.LocationMapper;
 import hr.tis.academy.sightseeingapp.mapper.UserMapper;
 import hr.tis.academy.sightseeingapp.model.Favourite;
+import hr.tis.academy.sightseeingapp.repository.AttractionRepository;
 import hr.tis.academy.sightseeingapp.repository.FavouriteRepository;
 import hr.tis.academy.sightseeingapp.repository.LocationRepository;
 import hr.tis.academy.sightseeingapp.repository.UserRepository;
@@ -22,19 +23,21 @@ import java.util.UUID;
 public class FavouriteServiceImpl implements FavouriteService {
 
     private final LocationMapper locationMapper;
-    private FavouriteMapper favouriteMapper;
-    private FavouriteRepository favouriteRepository;
-    private LocationRepository locationRepository;
-    private UserRepository userRepository;
-    private UserMapper userMapper;
+    private final FavouriteMapper favouriteMapper;
+    private final FavouriteRepository favouriteRepository;
+    private final LocationRepository locationRepository;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final AttractionRepository attractionRepository;
 
-    public FavouriteServiceImpl(FavouriteMapper favouriteMapper, FavouriteRepository favouriteRepository, LocationRepository locationRepository, UserRepository userRepository, UserMapper userMapper, LocationMapper locationMapper) {
+    public FavouriteServiceImpl(FavouriteMapper favouriteMapper, FavouriteRepository favouriteRepository, LocationRepository locationRepository, UserRepository userRepository, UserMapper userMapper, LocationMapper locationMapper, AttractionRepository attractionRepository) {
         this.favouriteMapper = favouriteMapper;
         this.favouriteRepository = favouriteRepository;
         this.locationRepository = locationRepository;
         this.userRepository = userRepository;
         this.userMapper = userMapper;
         this.locationMapper = locationMapper;
+        this.attractionRepository = attractionRepository;
     }
 
 
@@ -45,6 +48,7 @@ public class FavouriteServiceImpl implements FavouriteService {
             HttpHeaders headers = new HttpHeaders();
             headers.add("message", "Location does not exist.");
             headers.add("timestamp", LocalTime.now().toString());
+            headers.add("uuid",  UUID.randomUUID().toString());
             return ResponseEntity.status(HttpStatus.NOT_FOUND).headers(headers).body(null);
         }
 
@@ -52,6 +56,15 @@ public class FavouriteServiceImpl implements FavouriteService {
             HttpHeaders headers = new HttpHeaders();
             headers.add("message", "User does not exist.");
             headers.add("timestamp", LocalTime.now().toString());
+            headers.add("uuid",  UUID.randomUUID().toString());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).headers(headers).body(null);
+        }
+
+        if (!attractionRepository.existsByName(attractionName)) {
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("message", "Attraction does not exist.");
+            headers.add("timestamp", LocalTime.now().toString());
+            headers.add("uuid",  UUID.randomUUID().toString());
             return ResponseEntity.status(HttpStatus.NOT_FOUND).headers(headers).body(null);
         }
 
@@ -59,6 +72,7 @@ public class FavouriteServiceImpl implements FavouriteService {
             HttpHeaders headers = new HttpHeaders();
             headers.add("message", "Attraction already exists as a favourite.");
             headers.add("timestamp", LocalTime.now().toString());
+            headers.add("uuid",  UUID.randomUUID().toString());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
 
