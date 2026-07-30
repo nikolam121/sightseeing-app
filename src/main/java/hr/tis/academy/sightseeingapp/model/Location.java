d07f90f62f98d948fef20fd2dec713fff58fb6b6
Car <matijacarr@gmail.com>
2026-07-30 10:13:30 +0200

    Location i attraction

--- src/main/java/hr/tis/academy/sightseeingapp/model/Location.java
+++ src/main/java/hr/tis/academy/sightseeingapp/model/Location.java
@@ -1,4 +1,43 @@
 package hr.tis.academy.sightseeingapp.model;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "LOCATION", schema = "SIGHTSEEING")
 public class Location {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String locationName;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "ATTRACTION_ID")
+    private List<Attraction> attraction;
+
+    public Location(Long id, String locationName) {
+        this.id = id;
+        this.locationName = locationName;
+    }
+
+    public Location() {
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
 }
